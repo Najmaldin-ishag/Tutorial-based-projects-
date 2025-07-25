91351543288813a9d2e368f03d58766ddc1ac7b6
Najmaldin ishag <najmaldinosman@gmail.com>
2025-07-25 19:36:16 +0300

    update App.jsx

--- far-away/src/App.jsx
+++ far-away/src/App.jsx
@@ -1,3 +1,24 @@
+const initialItems = [
+  {
+    id: 1,
+    desc: "Passports",
+    quantity: 2,
+    packed: false,
+  },
+  {
+    id: 2,
+    desc: "Socks",
+    quantity: 12,
+    packed: false,
+  },
+  {
+    id: 3,
+    desc: "Charger",
+    quantity: 1,
+    packed: true,
+  },
+];
+
 function Logo() {
   return <h1>🏝️ Far Away</h1>;
 }
@@ -11,7 +32,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <span>
+        {item.quantity} {item.desc}
+      </span>
+      <button> ❌ </button>
+    </li>
+  );
 }
 
 function Stats() {
@@ -19,7 +59,6 @@ function Stats() {
     <footer>💼 You have X items on your list, and you already packed X</footer>
   );
 }
-
 
 const App = () => {
   return (
