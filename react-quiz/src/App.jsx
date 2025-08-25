0fa5eb290ac35e67ad46a3e3fe5d809647f4688c
Najmaldin ishag <najmaldinosman@gmail.com>
2025-08-25 19:06:30 +0300

    add Main component & install json-server package

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,9 +1,41 @@
-import DateCounter from "./Components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+
+const initialState = {
+  questions: [],
+
+  // "loading" "error" "ready" "active" "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("action unknown");
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8080/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 };
