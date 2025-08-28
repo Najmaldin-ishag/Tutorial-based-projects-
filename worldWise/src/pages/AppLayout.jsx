fbd75458563d9f3e995c7247a00437de55fa03d7
Najmaldin ishag <najmaldinosman@gmail.com>
2025-08-29 00:24:16 +0300

    implement nested routes in AppLayout.jsx

--- worldWise/src/pages/AppLayout.jsx
+++ worldWise/src/pages/AppLayout.jsx
@@ -1,5 +1,13 @@
+import Map from "../Components/Map";
+import Sidebar from "../Components/Sidebar";
+import styles from "./AppLayout.module.css";
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
+  );
 };
 
 export default AppLayout;
