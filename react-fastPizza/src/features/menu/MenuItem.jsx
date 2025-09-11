3002a14650953e1f552f99e7dab8f03ac16847ac
Najmaldin ishag <najmaldinosman@gmail.com>
2025-09-11 19:09:46 +0300

    implement Adding Menu Items to the Cart

--- react-fastPizza/src/features/menu/MenuItem.jsx
+++ react-fastPizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
+import { useDispatch } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
-
+import { addItem } from "../cart/cartSlice";
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
+  const dispatch = useDispatch();
+  function handleCart() {
+    const newItem = {
+      pizzaID: id,
+      name,
+      quantity: 1,
+      price: unitPrice,
+      totalPrice: unitPrice * 2,
+    };
+
+    dispatch(addItem(newItem));
+    console.log(newItem);
+  }
   return (
     <li>
       <img src={imageUrl} alt={name} />
@@ -11,6 +25,8 @@ function MenuItem({ pizza }) {
         <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+
+          {!soldOut && <button onClick={handleCart}>Add to cart</button>}
         </div>
       </div>
     </li>
