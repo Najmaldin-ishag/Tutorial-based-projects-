e244cbab22745f9bf3cacdec1578ed22c7f391a8
Najmaldin ishag <najmaldinosman@gmail.com>
2025-09-14 22:35:11 +0300

    finish setting up Pages and Routes

--- wild-oasis-dashboard-app/src/App.jsx
+++ wild-oasis-dashboard-app/src/App.jsx
@@ -1,20 +1,31 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
-import Button from "./ui/Button";
-
-const StyledApp = styled.div`
-  padding: 2rem;
-`;
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <Heading as="h6">App</Heading>
-      <Button variation="primary" size="small">
-        Click Me
-      </Button>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/cabins" element={<Cabins />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
