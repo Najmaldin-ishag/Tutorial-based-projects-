876297aab68606a3f6130c0afa6f97481d79dfcd
Najmaldin ishag <najmaldinosman@gmail.com>
2025-09-16 04:55:38 +0300

    implement Uploading Images to Supabase

--- wild-oasis-dashboard-app/src/services/apiCabins.js
+++ wild-oasis-dashboard-app/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("Cabins").select("*");
@@ -10,16 +10,53 @@ export async function getCabins() {
   return data;
 }
 
+/**
+ * Creates a new cabin entry in the "Cabins" table of the database.
+ *
+ * This function generates a unique image name for the cabin's image, constructs its public URL,
+ * and inserts the provided cabin data into the database using Supabase. If the insertion fails,
+ * it logs the error and throws an exception.
+ *
+ * @async
+ * @param {Object} newCabin - The new cabin data to be inserted, including an image file object.
+ * @returns {Promise<Object[]>} The inserted cabin data returned from the database.
+ * @throws {Error} If the cabin could not be created in the database.
+ *
+ * The function is designed this way to ensure that each cabin image has a unique name (to avoid
+ * filename collisions in storage) and to handle errors gracefully by logging and throwing them.
+ */
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imageUrl = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
   const { data, error } = await supabase
     .from("Cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imageUrl }])
     .select();
 
   if (error) {
     console.error("Cabins could not be created:", error);
     throw new Error("Cabins could not be created", error);
   }
+
+  // upload image
+
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  if (storageError) {
+    console.error(storageError);
+    await supabase.from("Cabins").delete().eq("id", data.id);
+    throw new Error(
+      "Image could not be uploaded and the cabin was not created",
+      storageError
+    );
+  }
+
   return data;
 }
 
--- wild-oasis-dashboard-app/src/services/supabase.js
+++ wild-oasis-dashboard-app/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vqnkhlidkqdthzpiqijd.supabase.co";
+export const supabaseUrl = "https://vqnkhlidkqdthzpiqijd.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
