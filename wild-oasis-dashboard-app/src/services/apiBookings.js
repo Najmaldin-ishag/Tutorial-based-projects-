2c11caa723d631ca8df3b17ef9f9552eb640cc26
Najmaldin ishag <najmaldinosman@gmail.com>
2025-09-27 17:58:26 +0300

    implement add sort to bookings

--- wild-oasis-dashboard-app/src/services/apiBookings.js
+++ wild-oasis-dashboard-app/src/services/apiBookings.js
@@ -9,12 +9,20 @@ export async function getBookings({ filter, sortBy }) {
     );
 
   // Filter
-  if (filter !== null) query = query.eq(filter.field, filter.value);
+  if (filter) query = query.eq(filter.field, filter.value);
   const { data, error } = await query;
+
+  // sort
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
+
   return data;
 }
 export async function getBooking(id) {
