64223b90b33570a6a0554ad069c4417ff8f71258
Najmaldin ishag <najmaldinosman@gmail.com>
2025-09-27 22:28:07 +0300

    implement pagination

--- wild-oasis-dashboard-app/src/services/apiBookings.js
+++ wild-oasis-dashboard-app/src/services/apiBookings.js
@@ -1,16 +1,17 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import { PAGE_SIZE } from "../utils/constant";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("Bookings")
     .select(
-      "id,created_at,startDate,endDate,numNights,numGuest,totalPrice,status, Cabins(name),  Guest(fullName,email)"
+      "id,created_at,startDate,endDate,numNights,numGuest,totalPrice,status, Cabins(name),  Guest(fullName,email)",
+      { count: "exact" }
     );
 
   // Filter
   if (filter) query = query.eq(filter.field, filter.value);
-  const { data, error } = await query;
 
   // sort
   if (sortBy)
@@ -18,12 +19,23 @@ export async function getBookings({ filter, sortBy }) {
       ascending: sortBy.direction === "asc",
     });
 
+  // Pagination
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
+
+  // Error Handling
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 export async function getBooking(id) {
   const { data, error } = await supabase
