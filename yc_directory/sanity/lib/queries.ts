7d5338436f55458eac0dedcb1c8d37fe02b68155
Najmaldin ishag <najmaldinosman@gmail.com>
2025-10-25 07:09:49 +0300

    feat:implement real-time search

--- yc_directory/sanity/lib/queries.ts
+++ yc_directory/sanity/lib/queries.ts
@@ -1,10 +1,10 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-  defineQuery(`*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
   _id, 
   title, 
-slug,
+  slug,
   _createdAt,
   author -> {
     _id, name, image, bio
