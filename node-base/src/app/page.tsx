4f4f0ac113fa96e0ee8835ac9641e84119ba0366
Najmaldin Ishag <najmaldinosman@gmail.com>
2026-02-09 04:39:31 +0200

    initial commit

---
+++ node-base/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <Button className="bg-primary text-primary-foreground hover:bg-primary/90">
+      Click me
+    </Button>
+  );
+};
+
+export default page;
